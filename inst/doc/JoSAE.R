be6265b81ff91de673abc527e52a6cb889aa05ad
Johannes Breidenbach <job@nibio.no>
2015-08-07 00:00:00 +0000

    version 0.2.3

--- inst/doc/JoSAE.R
+++ inst/doc/JoSAE.R
@@ -2,26 +2,26 @@
 ### Encoding: ISO8859-1
 
 ###################################################
-### code chunk number 1: JoSAE.Rnw:67-69 (eval = FALSE)
+### code chunk number 1: JoSAE.Rnw:68-70 (eval = FALSE)
 ###################################################
 ## install.packages("JoSAE")
 ## 
 
 
 ###################################################
-### code chunk number 2: JoSAE.Rnw:75-76
+### code chunk number 2: JoSAE.Rnw:76-77
 ###################################################
 library(JoSAE)
 
 
 ###################################################
-### code chunk number 3: JoSAE.Rnw:80-81 (eval = FALSE)
+### code chunk number 3: JoSAE.Rnw:81-82 (eval = FALSE)
 ###################################################
 ## ?JoSAE
 
 
 ###################################################
-### code chunk number 4: JoSAE.Rnw:100-110
+### code chunk number 4: JoSAE.Rnw:101-111
 ###################################################
 	#mean auxiliary variables for the populations in the domains
 data(JoSAE.domain.data)
@@ -36,7 +36,7 @@ print(xyplot(biomass.ha ~ mean.canopy.ht | domain.ID, data = JoSAE.sample.data))
 
 
 ###################################################
-### code chunk number 5: JoSAE.Rnw:131-134
+### code chunk number 5: JoSAE.Rnw:132-135
 ###################################################
     #lme model
 summary(fit.lme <- lme(biomass.ha ~ mean.canopy.ht, data=JoSAE.sample.data
@@ -44,7 +44,7 @@ summary(fit.lme <- lme(biomass.ha ~ mean.canopy.ht, data=JoSAE.sample.data
 
 
 ###################################################
-### code chunk number 6: JoSAE.Rnw:143-146
+### code chunk number 6: JoSAE.Rnw:144-147
 ###################################################
     #domain data need to have the same column names as sample data or vice versa
 d.data <- JoSAE.domain.data
@@ -52,13 +52,13 @@ names(d.data)[3] <- "mean.canopy.ht"
 
 
 ###################################################
-### code chunk number 7: JoSAE.Rnw:154-155
+### code chunk number 7: JoSAE.Rnw:155-156
 ###################################################
 result <- eblup.mse.f.wrap(domain.data = d.data, lme.obj = fit.lme)
 
 
 ###################################################
-### code chunk number 8: JoSAE.Rnw:165-174
+### code chunk number 8: JoSAE.Rnw:166-175
 ###################################################
 library(xtable)
 #tmp <- result[,c(1:2,6,4,9:11)]
@@ -72,7 +72,7 @@ print(xtable(tmp, label = "tab:two", caption="Number of population and sampled e
 
 
 ###################################################
-### code chunk number 9: JoSAE.Rnw:192-218
+### code chunk number 9: JoSAE.Rnw:193-219
 ###################################################
 
 tmp <- result[,c("biomass.ha.sample.mean", "Synth", "GREG", "EBLUP")]
@@ -103,7 +103,7 @@ legend(13,200, fill=grey(c(.3, .6, .8, .9)), legend=c("SRS", "Synth", "GREG", "E
 
 
 ###################################################
-### code chunk number 10: JoSAE.Rnw:236-252
+### code chunk number 10: JoSAE.Rnw:237-253
 ###################################################
 
 
@@ -124,7 +124,7 @@ landsat.sample <- merge(landsat.domains[4:5], tmp, by="CountyName")
 
 
 ###################################################
-### code chunk number 11: JoSAE.Rnw:258-265
+### code chunk number 11: JoSAE.Rnw:259-266
 ###################################################
 summary(landsat.lme <- lme(HACorn ~ PixelsCorn + PixelsSoybeans
                            , data=landsat.sample
@@ -136,7 +136,7 @@ result <- eblup.mse.f.wrap(domain.data = landsat.domains
 
 
 ###################################################
-### code chunk number 12: JoSAE.Rnw:268-278
+### code chunk number 12: JoSAE.Rnw:269-279
 ###################################################
 
 tmp <- result[,c("CountyName.domain","n.i.sample","EBLUP", "EBLUP.se.1", "EBLUP.se.2", "GREG.se")]
@@ -148,5 +148,67 @@ print(xtable(tmp, label = "tab:landsat"
       , include.rownames=FALSE)
 
 
+
+
+###################################################
+### code chunk number 13: JoSAE.Rnw:308-316
+###################################################
+data(nfi)
+    #fit the model
+fit.nfi.iw <- lm(vol.2011~Elev.Mean, nfi, weights=1/Elev.Mean)
+  #data (model matrix, X) of one validation stand
+stand <- cbind(Intercept=1, Elev.Mean=c(147.41,127.48,98.66,118.85,124,120.81,119.7),
+               N=c(0,23,0,55,27,80,56), E=c(73,77,0,39,37,54,54) )
+  #aggregate to obtain X-bar
+stand.agg <- apply(stand[,1:2], 2, mean)
+
+
+###################################################
+### code chunk number 14: JoSAE.Rnw:325-330
+###################################################
+    #obtain covariance matrix 
+Sigma <- vcov(fit.nfi.iw)
+
+    #residual variance
+sig <- summary(fit.nfi.iw)$sigma
+
+
+###################################################
+### code chunk number 15: JoSAE.Rnw:335-336
+###################################################
+var.p <- t(stand.agg) %*% Sigma %*% stand.agg
+
+
+###################################################
+### code chunk number 16: JoSAE.Rnw:342-343
+###################################################
+var.prh <- var.p + sum(sig^2 * stand[,2])/nrow(stand)^2
+
+
+###################################################
+### code chunk number 17: JoSAE.Rnw:348-350
+###################################################
+library(nlme)
+spG <- corGaus(23, form = ~N+E)
+
+
+###################################################
+### code chunk number 18: JoSAE.Rnw:356-359
+###################################################
+cormat <- corMatrix(Initialize(spG, data.frame(stand)))
+
+varmat <- (sig * sqrt(stand[,2])) %o% (sig * sqrt(stand[,2]))
+
+
+###################################################
+### code chunk number 19: JoSAE.Rnw:364-365
+###################################################
+var.prhs <- var.p + sum(cormat * varmat)/nrow(stand)^2
+
+
+###################################################
+### code chunk number 20: JoSAE.Rnw:371-372
+###################################################
+sqrt(c(var.p, var.prh, var.prhs))
 
 
